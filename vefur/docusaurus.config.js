3e3a5cc62b4e2d70a6f42150351a4a8742e7650b
Ernir <e.ernir@gmail.com>
2022-09-17 23:58:56 +0200

    Stilla til Docusaurus fyrir `gh-pages`

--- vefur/docusaurus.config.js
+++ vefur/docusaurus.config.js
@@ -6,18 +6,19 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'd20 á íslensku',
+  tagline: 'Á íslensku má alltaf finna svar',
+  url: 'https://ernir.github.io/d20-is/',
+  baseUrl: '/d20-is/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ernir', // Usually your GitHub org/user name.
+  projectName: 'd20-is', // Usually your repo name.
+  trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
