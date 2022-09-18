648f5512babf2ee68bfd42ea5c4812b8d6c13272
Eiríkur Ernir Þorsteinsson <e.ernir@gmail.com>
2022-09-18 15:40:18 +0200

    Bæta við vef fyrir verkefnið (#1)

---
+++ vefur/docs/intro.md
@@ -0,0 +1,28 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# d20 á íslensku
+
+Hér getur að líta fimmtu útgáfu d20 kerfisins á íslensku, fyrir hópa sem vilja spila spunaspil á ástkæra ylhýra.
+
+## Hvað má finna hér?
+
+[d20 kerfisvísunin](https://5e.d20srd.org/) (e. _d20 system reference document_) sem gefin hefur verið út með opnu leyfi er þýdd úr ensku. Lokamarkmið þessa verkefnis er að þýða alla kerfisvísunina.
+
+Athugið þó að kerfisvísunin er ekki nægileg til að hefja leik ein og sér. d20 kerfið sem hér er þýtt er byggt á fimmtu útgáfu Dungeons & Dragons hlutverkaspilsins, sem er í eigu fyrirtækisins Wizards of the Coast. Nokkrum lykilhlutum þess spils, t.d. reglum um persónusköpun, er vísvitandi sleppt úr kerfisvísuninni. Bækurnar, sem ekki hafa verið þýddar á íslensku, eru enn nauðsynlegar til að fá heildstæða mynd af reglunum.
+
+Hér er markmiðið því ekki að setja fram sjálfstætt hlutverkaspil, heldur að hjálpa reyndum íslenskum spunaspilurum að halda spil sín á íslensku með því að setja fram heildstæða þýðingu á grunnhugtökum.
+
+## Hvaðan koma þýðingarnar?
+
+Uppruna flestra hugtakanna má rekja til [Orðasafns íslenskra spunaspilara](https://docs.google.com/spreadsheets/d/1-k4bClHJhkCKrDG0jxg8KKn81oqiOGXaJHhjZ9cFH2c/edit#gid=0), töfluskjals þar sem þýðingum á spunaspilstengdum orðum hefur verið safnað frá íslenska spunaspilssamfélaginu um árabil.
+
+Eðli málsins samkvæmt leiðir hópsöfnun sem slík til þess að margar góðar þýðingar verða til á flestum hugtökum. Hér hefur "ritstjóri" verkefnisins handvalið þýðingar úr sarpinum.
+
+## Hvernig get ég lagt hönd á plóg?
+
+Þýðingarnar má finna á einföldu, tölvutæku Markdown-sniði á [Github](https://github.com/Ernir/d20-is) síðu verkefnisins, þar sem þeim má breyta að vild. Viðbætur og lagfæringar eru vel þegnar.
+
+Eiríkur Ernir Þorsteinsson sér um vefinn, hægt er að hafa samband á e.ernir@gmail.com.
---
+++ vefur/docusaurus.config.js
@@ -0,0 +1,100 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'd20 á íslensku',
+  tagline: 'Fyrir hópa sem vilja spila spunaspil á ástkæra ylhýra',
+  url: 'https://ernir.github.io',
+  baseUrl: '/d20-is/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/book.png',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ernir', // Usually your GitHub org/user name.
+  projectName: 'd20-is', // Usually your repo name.
+  trailingSlash: false,
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'is',
+    locales: ['is'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/'
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'd20 á íslensku',
+        logo: {
+          alt: 'Mynd af bók',
+          src: 'img/book.png',
+        },
+        items: [
+          {
+            href: 'https://github.com/Ernir/d20-is',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Tengt efni á íslensku',
+            items: [
+              {
+                label: 'Orðasafn íslenskra spunaspilara - Google Sheet',
+                href: 'https://docs.google.com/spreadsheets/d/1-k4bClHJhkCKrDG0jxg8KKn81oqiOGXaJHhjZ9cFH2c/edit#gid=0',
+              },
+              {
+                label: 'Orðasafn íslenskra spunaspilara - vefsíða',
+                href: 'http://ordasafnspunaspilara.info/',
+              }
+            ],
+          },
+          {
+            title: 'd20 á ensku',
+            items: [
+              {
+                label: 'd20 kerfisvísunin á ensku',
+                href: 'https://5e.d20srd.org/'
+              }
+            ]
+          }
+        ],
+        copyright: `Höfundarréttur © ${new Date().getFullYear()} Eiríkur Ernir Þorsteinsson`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
